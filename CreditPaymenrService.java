95306fee20d08bec5d77cc0ef8c098fc1d046fd1
AlexandraPyaterikova <78533399+AlexandraPyaterikova@users.noreply.github.com>
2021-03-12 19:15:56 +0300

    formated

--- CreditPaymenrService.java
+++ CreditPaymenrService.java
@@ -1,13 +1,13 @@
 public class CreditPaymentService {
     public int calculate(float sum, float term) {
-        float rate = 9.99F / 100F / 12F;
-        float a = (1F+rate);
-        double a1 = Math.pow(a,term);
+        float rate = 9.99 F / 100 F / 12 F;
+        float a = (1 F + rate);
+        double a1 = Math.pow(a, term);
         double a2 = rate * a1;
-        float b = (1F + rate);
+        float b = (1 F + rate);
         double b1 = Math.pow(b, term);
-        double b2 = b1 - 1F;
-        int payment = (int) (a2 / b2 * sum);
+        double b2 = b1 - 1 F;
+        int payment = (int)(a2 / b2 * sum);
         return payment;
     }
 }
