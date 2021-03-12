0d7e5bc24a3558807fab3230413fa608dd0f22f1
AlexandraPyaterikova <AlexandraPyaterikova@yandex.ru>
2021-03-12 18:51:46 +0300

    CreditPaymentService

---
+++ CreditPaymenrService.java
@@ -0,0 +1,13 @@
+public class CreditPaymentService {
+    public int calculate(float sum, float term) {
+        float rate = 9.99F / 100F / 12F;
+        float a = (1F+rate);
+        double a1 = Math.pow(a,term);
+        double a2 = rate * a1;
+        float b = (1F + rate);
+        double b1 = Math.pow(b, term);
+        double b2 = b1 - 1F;
+        int payment = (int) (a2 / b2 * sum);
+        return payment;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int oneYear = service.calculate(1000000F, 12F);
+        System.out.println("Ежемесячный платеж при кредитовании на 1 год: " + oneYear + " руб.");
+
+        int twoYear = service.calculate(1000000F, 24F);
+        System.out.println("Ежемесячный платеж при кредитовании на 2 года: " + twoYear + " руб.");
+
+        int threeYear = service.calculate(1000000F, 36F);
+        System.out.println("Ежемесячный платеж при кредитовании на 3 года: " + threeYear + " руб.");
+    }
+}
