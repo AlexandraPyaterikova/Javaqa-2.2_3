ea3a26a5fc71d1275a9dc31aa27a853a43a381cb
AlexandraPyaterikova <78533399+AlexandraPyaterikova@users.noreply.github.com>
2021-03-15 12:35:17 +0300

    Update CreditPaymenrService.java

--- CreditPaymenrService.java
+++ CreditPaymenrService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(float sum, float term) {
-        float rate = 9.99F / 100F / term;
+        float rate = 9.99F / 100F / 12F;
         float a = (1F+rate);
         double a1 = Math.pow(a,term);
         double a2 = rate * a1;
